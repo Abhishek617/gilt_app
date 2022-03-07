82860c6ba5c99c0b08dc166533c7d2752e1ad18d
iabhishekx <ravixu34@gmail.com>
2022-03-07 18:41:16 +0530

    Working Round button

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
-
 import 'package:guilt_app/ui/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'di/components/service_locator.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setPreferredOrientations();
