ec27ea85cdf1df38d962e42ee244292834bd5327
Mital Joshi <mital@phpdots.com>
2022-03-08 11:37:23 +0530

    Reset Password Changes

--- lib/ui/forgot_reset_password/reset_password.dart
+++ lib/ui/forgot_reset_password/reset_password.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:guilt_app/constants/dimens.dart';
 import 'package:guilt_app/utils/routes/routes.dart';
 import 'package:guilt_app/widgets/rounded_button_widget.dart';
 import 'package:guilt_app/widgets/textfield_widget.dart';
@@ -16,7 +17,6 @@ class _Reset_passwordState extends State<Reset_password> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 70,
         leading: Icon(
           Icons.arrow_back_ios_outlined,color: Colors.black, size: 15,),
         backgroundColor: Colors.transparent,
@@ -25,64 +25,56 @@ class _Reset_passwordState extends State<Reset_password> {
       body: Center(
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: Container(
-                  width: 140.0,
-                  height: 140.0,
-                  decoration: new BoxDecoration(
-                    color: Colors.blue,
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: NetworkImage("https://cdn-icons-png.flaticon.com/512/3135/3135715.png"),
-                      fit: BoxFit.cover,
-                    ),
+              Container(
+                width: 140.0,
+                height: 140.0,
+                margin: EdgeInsets.symmetric(vertical: Dimens.vertical_padding),
+                decoration: new BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage("https://cdn-icons-png.flaticon.com/512/3135/3135715.png"),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only( top: 15),
-                child: Text('Guilt App', style: TextStyle(fontSize: 19.0,),),
-              ),
+                  padding: EdgeInsets.only(top: Dimens.vertical_padding,bottom: 50),
+                  child: Text('Guilt App', style: TextStyle(fontSize: 19.0,),)),
               Padding(
-                padding: const EdgeInsets.only(top: 20),
+                padding: EdgeInsets.only(top: Dimens.vertical_padding),
                 child: Text('free guide will tell about the impact your gift will have', style: TextStyle(fontSize: 13.0,),),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Container(
-                  height: 50,
-                  width: 320,
-
-                  child:
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter Password',
-                    ),
+              Container(
+                height: 50,
+                width: 320,
+                margin: EdgeInsets.symmetric(vertical: Dimens.vertical_padding),
+                child:
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter Password',
                   ),
-
                 ),
               ),
-               Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                    child: Container(
-                      height: 50,
-                      width: 320,
-                      child: ElevatedButtonWidget(
-                        buttonText: 'Reset Password',
-                          buttonColor: Colors.orangeAccent,
-                          textColor: Colors.black,
-                          onPressed: (){
-                            Navigator.of(context).pushNamedAndRemoveUntil(
-                                Routes.otp, (Route<dynamic> route) => false);
-                          },
-                          ),
-                    ),
-                ),
+               Container(
+                 height: 50,
+                 width: 320,
+                 margin: EdgeInsets.symmetric(vertical: Dimens.vertical_padding),
+                 child: ElevatedButtonWidget(
+                   buttonText: 'Reset Password',
+                     buttonColor: Colors.orangeAccent,
+                     textColor: Colors.black,
+                     onPressed: (){
+                       Navigator.of(context).pushNamedAndRemoveUntil(
+                           Routes.otp, (Route<dynamic> route) => false);
+                     },
+                     ),
+               ),
             ],
           ),
         ),
