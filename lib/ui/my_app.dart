3f2909ceab1e700124a81e75377257595ce2bf98
iabhishekx <ravixu34@gmail.com>
2022-03-09 19:27:26 +0530

    Scusses screens

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -42,26 +42,29 @@ class MyApp extends StatelessWidget {
       child: Observer(
         name: 'global-observer',
         builder: (context) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: Strings.appName,
-            theme: _themeStore.darkMode ? themeDataDark : themeData,
-            routes: Routes.routes,
-            locale: Locale(_languageStore.locale),
-            supportedLocales: _languageStore.supportedLanguages
-                .map((language) => Locale(language.locale!, language.code))
-                .toList(),
-            localizationsDelegates: [
-              // A class which loads the translations from JSON files
-              AppLocalizations.delegate,
-              // Built-in localization of basic text for Material widgets
-              GlobalMaterialLocalizations.delegate,
-              // Built-in localization for text direction LTR/RTL
-              GlobalWidgetsLocalizations.delegate,
-              // Built-in localization of basic text for Cupertino widgets
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            home:(_userStore.isFirst ? OnBoardingPage() : (_userStore.isLoggedIn ? HomeScreen() : WelcomeLogin())),
+          return GestureDetector(
+            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: Strings.appName,
+              theme: _themeStore.darkMode ? themeDataDark : themeData,
+              routes: Routes.routes,
+              locale: Locale(_languageStore.locale),
+              supportedLocales: _languageStore.supportedLanguages
+                  .map((language) => Locale(language.locale!, language.code))
+                  .toList(),
+              localizationsDelegates: [
+                // A class which loads the translations from JSON files
+                AppLocalizations.delegate,
+                // Built-in localization of basic text for Material widgets
+                GlobalMaterialLocalizations.delegate,
+                // Built-in localization for text direction LTR/RTL
+                GlobalWidgetsLocalizations.delegate,
+                // Built-in localization of basic text for Cupertino widgets
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              home:(_userStore.isFirst ? OnBoardingPage() : (_userStore.isLoggedIn ? HomeScreen() : WelcomeLogin())),
+            ),
           );
         },
       ),
