bc1a96e465e7f15d6090c12661dc731e3a48cfa5
Mital Joshi <jrathod@limmedia.io>
2022-06-07 16:56:19 +0530

    Added Map Search bar

--- lib/ui/Map/map.dart
+++ lib/ui/Map/map.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoding/geocoding.dart';
+import 'dart:async';
+import 'package:google_api_headers/google_api_headers.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:guilt_app/constants/app_settings.dart';
 
 class SetOnMapScreen extends StatefulWidget {
   @override
@@ -10,8 +15,9 @@ class SetOnMapScreen extends StatefulWidget {
 class _MapState extends State<SetOnMapScreen> {
   late String selectedAddress;
   late GoogleMapController _controller;
-  late Marker MyMarker = new Marker(markerId: MarkerId('Address_Marker'));
-  List<Location> addressList = [];
+  late Marker myMarker = new Marker(markerId: MarkerId('Address_Marker'));
+  List<dynamic> addressList = [];
+  Mode _mode = Mode.overlay;
   late PageController _pageController;
   late int prevPage;
   late final ArgumentCallback<LatLng> onTap;
@@ -43,8 +49,7 @@ class _MapState extends State<SetOnMapScreen> {
   }
 
   getPredictions() async {
-    List<Location> locations =
-        await locationFromAddress(selectedAddress ?? "Gronausest10, Enschede");
+    List<dynamic> locations = await locationFromAddress(selectedAddress);
     setState(() {
       addressList = locations;
       print(addressList);
@@ -54,10 +59,10 @@ class _MapState extends State<SetOnMapScreen> {
   }
 
   getAddressFromLocation() async {
-    List<Placemark> placemarks = await placemarkFromCoordinates(
+    List<Placemark> placeMarks = await placemarkFromCoordinates(
         currentPos.latitude, currentPos.longitude);
     setState(() {
-      Placemark add = placemarks[0];
+      Placemark add = placeMarks[0];
       selectedAddress = (add.name ?? '') +
           ',' +
           (add.subLocality ?? '') +
@@ -71,22 +76,104 @@ class _MapState extends State<SetOnMapScreen> {
   }
 
   addMarker(LatLng markerPosition) {
-    currentPos = markerPosition;
-    getAddressFromLocation();
-    MyMarker = Marker(
-        markerId: MarkerId('Address_Marker'),
-        draggable: true,
-        infoWindow: InfoWindow(title: 'Address', snippet: selectedAddress),
-        position: markerPosition);
-    _pageController = PageController(initialPage: 1, viewportFraction: 0.8)
-      ..addListener(_onScroll);
+    try {
+      currentPos = markerPosition;
+      getAddressFromLocation();
+      myMarker = Marker(
+          markerId: MarkerId('Address_Marker'),
+          draggable: true,
+          infoWindow: InfoWindow(title: 'Address', snippet: selectedAddress),
+          position: markerPosition);
+      _pageController = PageController(initialPage: 1, viewportFraction: 0.8)
+        ..addListener(_onScroll);
+      moveCamera();
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  Widget _buildDropdownMenu() => DropdownButton(
+        value: _mode,
+        items: <DropdownMenuItem<Mode>>[
+          DropdownMenuItem<Mode>(
+            child: Text("Overlay"),
+            value: Mode.overlay,
+          ),
+          DropdownMenuItem<Mode>(
+            child: Text("Fullscreen"),
+            value: Mode.fullscreen,
+          ),
+        ],
+        onChanged: (m) {
+          setState(() {
+            _mode = m as Mode;
+          });
+        },
+      );
+
+  void onError(PlacesAutocompleteResponse response) {
+    print(response);
+  }
+
+  Future<void> _handlePressButton() async {
+    try {
+      // show input autocomplete with selected mode
+      // then get the Prediction selected
+      Prediction? p = await PlacesAutocomplete.show(
+        context: context,
+        types: [],
+        strictbounds: false,
+        apiKey: AppSettings.googleMapAPIKey,
+        onError: onError,
+        mode: _mode,
+        decoration: InputDecoration(
+          hintText: 'Search',
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        components: [],
+      );
+
+      displayPrediction(p!);
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  Future<Null> displayPrediction(Prediction p) async {
+    try {
+      if (p != null) {
+        // get detail (lat/lng)
+        GoogleMapsPlaces _places = GoogleMapsPlaces(
+          apiKey: AppSettings.googleMapAPIKey,
+          apiHeaders: await GoogleApiHeaders().getHeaders(),
+        );
+        PlacesDetailsResponse detail =
+            await _places.getDetailsByPlaceId(p.placeId!);
+        final lat = detail.result.geometry?.location.lat;
+        final lng = detail.result.geometry?.location.lng;
+        setState(() {
+          addMarker(LatLng(lat!, lng!));
+        });
+      }
+    } catch (err) {
+      print(err);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Event Location"),
+        actions: [ IconButton(
+          onPressed: _handlePressButton,
+          icon: Icon(Icons.search),
+        ),],
+        title: Text('Map'),
         centerTitle: true,
         leading: IconButton(
           onPressed: () {
@@ -112,7 +199,7 @@ class _MapState extends State<SetOnMapScreen> {
                         target: LatLng(
                             addressList[0].latitude, addressList[0].longitude),
                         zoom: 13.0),
-                    markers: Set.from([MyMarker]),
+                    markers: Set.from([myMarker]),
                     onMapCreated: mapCreated,
                   )
                 : Center(
@@ -131,11 +218,11 @@ class _MapState extends State<SetOnMapScreen> {
   }
 
   moveCamera() {
-    // _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-    //   target: hospitalInfo[_pageController.page!.toInt()].locationCoords,
-    //   zoom: 14.0,
-    //   bearing: 45.0,
-    //   tilt: 45.0,
-    // )));
+    _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: currentPos,
+      zoom: 14.0,
+      bearing: 45.0,
+      tilt: 45.0,
+    )));
   }
 }
