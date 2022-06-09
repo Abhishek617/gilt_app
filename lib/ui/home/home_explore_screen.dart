7484fd43f48e4268ea1ac74d2923b1c577c50dbb
Mital Joshi <jrathod@limmedia.io>
2022-06-09 16:43:00 +0530

    Fixes API call on keyboard activity and added static bank account details for testing

--- lib/ui/home/home_explore_screen.dart
+++ lib/ui/home/home_explore_screen.dart
@@ -47,9 +47,16 @@ class _HomeExploreScreenState extends State<HomeExploreScreen> {
   @override
   void didChangeDependencies() {
     if (this.mounted) {
-      initSetup();
-      G.initSocket();
-      G.socketUtils.initSocket();
+      var route = ModalRoute.of(context);
+      if(route!=null){
+        var mRoute = route.settings.name;
+        if(mRoute == Routes.explore_home){
+          initSetup();
+          G.initSocket();
+          G.socketUtils.initSocket();
+        }
+      }
+
       super.didChangeDependencies();
     }
   }
