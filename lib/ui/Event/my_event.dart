c187fdd158e475e991ace87b57c50ea9a18a4a1d
SathvaraChitra <107401657+SathvaraChitra@users.noreply.github.com>
2022-06-23 10:40:32 +0530

    Merge latest code with jay

--- lib/ui/Event/my_event.dart
+++ lib/ui/Event/my_event.dart
@@ -133,6 +133,7 @@ class _MyEventState extends State<MyEvent> {
                             ),
                           ),
                           Padding(
+
                               padding:
                               EdgeInsets.only(top: 5, left: 100, right: 00),
                               child: Container(
